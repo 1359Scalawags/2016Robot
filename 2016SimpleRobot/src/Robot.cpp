a8306e5a3133d719425cc981ab671548e0877d76
Bradley Karkanen <bradleykarkanen@gmail.com>
2016-01-31 13:30:22 -0800

    Add comments to make code more human readable

--- 2016SimpleRobot/src/Robot.cpp
+++ 2016SimpleRobot/src/Robot.cpp
@@ -9,17 +9,29 @@ class Robot: public SampleRobot
 {
 
 	RobotDrive chassis; // robot drive system
+
+	//joysticks for control
 	Joystick leftstick;
 	Joystick rightstick;
+
+	//controls whether or not the robbot can go forward
 	bool driveforward;
+
+	//encoders
 	Encoder leftencoder;
 	Encoder rightencoder;
+
+	//speed controllers
 	VictorSP leftA;
 	//VictorSP leftB;
 	VictorSP rightA;
 	//VictorSP rightB;
+
+	//variables for camera
 	LiveWindow *lw;
 	int autoLoopCounter;
+
+	//objects for the other systems on the robot
 	BallHandler ballhandler;
 	Lift lift;
 
@@ -54,7 +66,7 @@ public:
 		}
 	}
 
-	void setDriveSpeed()
+	void setDriveSpeed() //take input from the joysticks and sets the drive speed
 	{
 		float leftstickvalue = -getJoystickTransform(leftstick.GetY());
 		float rightstickvalue = -getJoystickTransform(rightstick.GetY());
@@ -67,15 +79,18 @@ public:
 
 	float getJoystickTransform(float input)
 	{
+		//for adjustment of speed profile
+		//for now does nothing only passes code through
 		return input;
 	}
 
 	void RobotInit()
 		{
+			//camera stuff
 			lw = LiveWindow::GetInstance();
 		}
 
-	void Autonomous()
+	void Autonomous() //controls autonomous
 	{
 		while(IsAutonomous() && IsEnabled())
 		{
