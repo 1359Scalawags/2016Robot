4716da6b4c991464d3e9dbdadba7d97645961a6c
Bradley Karkanen <bradleykarkanen@gmail.com>
2016-01-31 15:53:49 -0800

    Added methods for more automatic control of ballhandler

--- 2016SimpleRobot/src/BallHandler.cpp
+++ 2016SimpleRobot/src/BallHandler.cpp
@@ -8,25 +8,30 @@ const int HANDLER_FLIP_DOWN = 2;
 const int HANDLER_BALL_IN = 4;
 const int HANDLER_BALL_OUT = 5;
 const int HANDLER_ON_OFF = 1;
+const int BALLSWITCH = 7;
 
 
 class BallHandler
 {
 private:
-	bool ballsensor; //used to enable/disable the ball handler automatically
+
 	VictorSP drive;
 	VictorSP flipper; //use left joystick for ball handling buttons
 	VictorSP arm;
 	Joystick ballhandler;
 	bool flipped = true;
+	DigitalInput ballsensor; //used to enable/disable the ball handler automatically
+	bool switched;
 
 public:
 
-	BallHandler() : ballsensor(BALL_SENSOR),
+	BallHandler() :
 				drive(LIFT_MOTOR),
 				flipper(LIFT_FLIP),
 				arm(LIFT_ARM),
-				ballhandler(LEFT_JOYSTICK_PORT)
+				ballhandler(LEFT_JOYSTICK_PORT),
+				ballsensor(BALLSWITCH),
+				switched(false)
 
 	{
 
@@ -35,8 +40,8 @@ public:
 	void update()
 	{
 
-		flip();
-		fire();
+		flip(); //controls flipping
+		fire(); //controls firing
 
 	}
 
@@ -60,18 +65,41 @@ public:
 		return flipped;
 	}
 
+	void enableHandlerDrive()
+	{
+		if(ballhandler.GetRawButton(HANDLER_ON_OFF) == true && flipped == false)
+		{
+			//if ballhandler is enabled while flipped up it will automatically flip down
+			flipdown();
+			handleron();
+
+		}
+		else if(ballhandler.GetRawButton(HANDLER_ON_OFF) == true && flipped == true)
+		{
+			handleron();
+		}
+
+	}
 	void fire()
 	{
 
 		//control ball firing
-		if(ballhandler.GetRawButton(HANDLER_BALL_IN) == true && ballsensor == false)
+		if(ballhandler.GetRawButton(HANDLER_BALL_IN) == true && ballsensor.Get() == false)
 		{
 			//Allow the Handler to be turned on to take a ball in
+
+
+
+			disablehandler();
 		}
 
-		else if(ballhandler.GetRawButton(HANDLER_BALL_OUT) && ballsensor == true)
+		else if(ballhandler.GetRawButton(HANDLER_BALL_OUT) && ballsensor.Get() == true)
 		{
 			//Allow the ball to be fired only if a ball is present in the Handler
+			if(ballsensor.Get() == true)
+			{
+				drive.Set(-0.75f);
+			}
 		}
 	}
 
@@ -83,5 +111,19 @@ public:
 	void flipdown()
 	{
 		//flip ball handler down only if it is already up
+	}
+
+	void handleron()
+	{
+		//powers on the ballhandler's belts
+		drive.Set(0.75f);
+	}
+	void disablehandler()
+	{
+		//Disables the ballhandler when a ball is present
+		if(ballsensor.Get() == true)
+		{
+			drive.Set(0);
+		}
 	}
 };
