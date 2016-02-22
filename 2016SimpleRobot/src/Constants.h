fc3299eb3bebd04867a8e330e518b2711f36f586
Bradley Karkanen <bradleykarkanen@gmail.com>
2016-02-21 16:38:53 -0800

    Fixed some Constants

--- 2016SimpleRobot/src/Constants.h
+++ 2016SimpleRobot/src/Constants.h
@@ -16,8 +16,14 @@
 	const int RIGHT_CHANNEL_B = 22; //extender board
 	const int LEFT_CHANNEL_A = 23; //extender board
 	const int LEFT_CHANNEL_B = 24; //extender board
+
+	//limit switches
+	const int HANDLER_LIMIT_UP = 2;
+	const int HANDLER_LIMIT_DOWN = 1;
+	const int BALLSWITCH = 0; //this is the BallSensor
 	const int ARM_LIMIT_OUT = 3;
 	const int ARM_LIMIT_IN = 4;
+	const int LIFT_SENSOR = 5;
 
 
 //PWM
@@ -28,7 +34,9 @@
 	const int RIGHTB_MOTOR_ID = 3;
 	const int LIFT_MOTOR = 6; //Talon
 	const int ARM_POSITION_MOTOR = 4;
-
+	const int SPIN_MOTOR = 5; //the motor the spins the grabber roller
+	//Servo that may be on the lift
+	const int SERVO_MOTOR_ID = 17;
 
 //Relay
 	const int BALL_HANDLER_MOTOR = 1; //relay
@@ -55,12 +63,11 @@
 	const int LIFT_BUTTON_UP = 3;
 	const int LIFT_BUTTON_DOWN = 2;
 
-//Servo that may be on the lift
-	const int SERVO_MOTOR_ID = 17;
+
 
 	const bool IFLIFT = true;
 
-	const int LIFT_SENSOR = 5;
+
 
 
 
@@ -69,7 +76,7 @@
 	//controls the direction of the Handler's belts
 		const int HANDLER_BALL_IN = 4;
 		const int HANDLER_BALL_OUT = 5;
-		const int HANDLER_ON_OFF = 1;
+
 
 	//buttons for controlling the Handler's position
 		const int HANDLER_UP_BUTTON = 11;
@@ -77,27 +84,20 @@
 		const int HANDLER_DOWN_BUTTON = 10;
 		const int HANDLER_GRAB = 2; //puts the handler in grab mode
 		const int HANDLER_SHOOT =3; //fires the ball
-
-	//limit switches
-		const int HANDLER_LIMIT_UP = 2;
-		const int HANDLER_LIMIT_DOWN = 1;
-		const int BALLSWITCH = 0; //this is the BallSensor
-
-
-	//motors for the Handler
-
-
-
-//constants for Arm
-
-	//limit switches
-
-	//Motors for the Arm
-		const int SPIN_MOTOR = 5; //the motor the spins the grabber roller
-
 	//Buttons for moving Arm in and out
-		const int ARM_OUT_BUTTON = 4;
-		const int ARM_IN_BUTTON = 5;
+		const int ARM_OUT_BUTTON = 7;
+		const int ARM_IN_BUTTON = 6;
+
+
+
+
+
+
+
+
+
+
+
 
 const int TEST_MODE_BUTTON = 9;
 
